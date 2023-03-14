0744ed00764bceea80670fd7dcc134fd34bc85b4
Alex <baileymk@hotmail.com>
2023-03-14 19:53:27 +0000

    readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Metrics API
+
+An API that returns server metrics using Express Prometheus Middleware
+
+## Installation
+
+To install this app, simply clone it down to your local machine:
+
+```bash
+git clone https://github.com/Bailey30/MetricsAPI change_repo_name
+```
+
+Once you have cloned the app, navigate to the app's root directory:
+
+```bash
+cd change_repo_name
+```
+
+Then, install the app's dependencies using npm:
+
+```bash
+npm install
+```
+
+## Running the app
+
+To run the app, use the following command:
+
+```bash
+npm start
+```
+
+This will start a development server and open the app in your default browser. If the app doesn't automatically open, navigate to http://localhost:3001 in your browser.
--- dist/server.js
+++ dist/server.js
@@ -1,37 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-var port = 3001;
-var body_parser_1 = __importDefault(require("body-parser"));
-var routes_1 = __importDefault(require("./routes"));
 var cors = require("cors");
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-// define a route handler for the default home pagelet
-var corsOptions = {
-    origin: [
-        "http://localhost:3001",
-        "http://localhost:3000",
-        "https://clear-start-crm.opopservices.co.uk",
-        "https://f12.opopservices.co.uk",
-    ],
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-    optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.use(routes_1.default);
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+var express = require("express");
+var promMid = require("express-prometheus-middleware");
+var app = express();
+app.use(cors());
+var PORT = 3001;
+app.use(promMid({
+    metricsPath: "/metrics",
+    collectDefaultMetrics: true, // collect default metrics
+}));
+// if req doesnt have autorization header then return 403
+app.use(function (req, res, next) {
+    var bearerHeader = req.headers["authorization"];
+    if (bearerHeader !== "Bearer mysecrettoken") {
+        res.sendStatus(403);
+    }
+    else {
+        next();
+    }
 });
-// start the express server
-app.listen(port !== null && port !== void 0 ? port : 3001, function () {
-    console.log('server started at http://localhost:3000');
+app.get("/time", function (req, res) {
+    var epoch = Math.floor(new Date().getTime() / 1000);
+    res.json({
+        epoch: epoch,
+    });
 });
-// run the server
-// npm run dev
-// open the browser and go to http://localhost:3000
-// you should see the message "Hello world!"
+app.listen(PORT, function () {
+    console.log("Example api is listening on http://localhost:".concat(PORT));
+});
